95ddb49a3755aa4e471c37374b1635bbd9fc6ca6
Jfishin <joshuagrgurich9287@gmai.com>
2026-02-11 16:25:57 -0500

    Initial Commit

---
+++ EXEmacRPC/EXEmacRPCApp.swift
@@ -0,0 +1,17 @@
+//
+//  EXEmacRPCApp.swift
+//  EXEmacRPC
+//
+//  Created by Jfishin on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct EXEmacRPCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
